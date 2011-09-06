20cae7538d585a1ece683f9a98836f66fa1e6f27
aramisf <aramis.fernandes@gmail.com>
2011-09-06 00:22:28 -0300

    Added idle test program.
    This program generates a child process that keeps running on the background,
    and treats every treatable signal sent to it. One can use pkill program to test.

---
+++ c/idle.c
@@ -0,0 +1,42 @@
+/* Este programa solta um processo filho e finaliza o pai, o filho fica
+ * inativo ateh receber um sinal, e quando o receber, chama uma funcao que
+ * imprime na tela o valor do sinal recebido 
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+
+void *saida (void) {
+	printf("\nSaindo...\n");
+	exit(107);
+}
+
+void trata_sinal (int sinal) {
+
+    /* Alguns sinais sao intrataveis */
+	if (sinal != 15)
+		printf ("\nRecebi sinal: %d\n",sinal);
+	else
+		atexit(saida());
+}
+
+int main (int argc, char **argv) {
+
+	int i;
+	int pid;
+	pid = fork();
+	
+	if (pid != 0) exit(0);
+
+	if (pid == 0) {
+
+		for (i=0; i<=64; i++)
+			signal(i,trata_sinal);
+
+		while (1) sleep(3);
+		
+	}
+	return 0;
+}
