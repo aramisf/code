b6029e99ad42cf3d0a8812ba4d5823b3c6061706
aramisf <aramis.fernandes@gmail.com>
2011-09-06 00:32:16 -0300

    More comments
    Also changed a loop value to show some more numbers and its multiples.

--- c/shift.c
+++ c/shift.c
@@ -9,11 +9,13 @@ int main () {
     /* Demonstracao do operador shift */
     printf("normal: %d <<1: %d <<2: %d >>1: %d >>2: %d\n",a,a<<1,a<<2,a>>1,a>>2);
 
+    /* Multiplos de 3 */
     for (i=0; i<10; i++) {
         printf ("i: %d -- %d\n",i, i & 0x3);
     }
 
-    for (i=0; i<25; i++){
+    /* Multiplos de 15 */
+    for (i=0; i<31; i++){
         printf ("i: %d -- %d\n",i, i & 0xf);
     }
     return 0;
