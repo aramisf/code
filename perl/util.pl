6949b8b37b8c393b7368e4506ba7fe70e7be6c23
aramisf <aramis.fernandes@gmail.com>
2011-09-12 10:04:23 -0300

    Adicionado um futuro modulo

---
+++ perl/util.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl -w
+# util.pl - Funcoes de uso comum. A ideia eh que este arquivo seja um modulo e
+# q seja possivel importa-lo para uso em outros programas.
+# aramisf@github.com
+
+sub separador {
+
+    print "- - -\n";
+}
