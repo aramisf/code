39c605e0dc3aa8c2706a0965fc34042fbc005574
aramisf <aramis.fernandes@gmail.com>
2011-09-12 10:05:51 -0300

    Adicionados alguns exemplos

--- perl/strings.pl
+++ perl/strings.pl
@@ -21,6 +21,21 @@ sub main
     my $s2 = qq:frase 2, $aspasd:;
     message($s1);
     message($s2);
+
+    print "- - -\n";
+
+    # Mais alguns exemplos:
+    my $st1 = "hi ";
+    my $multiplier = "5";
+    my $rep = $st1 x $multiplier;
+    print "$rep\n";
+    
+    # Apenas para olhar o q aparece na saida (pq nao vai imprimir o esperado)
+    # pois ha um erro no operador *, que soh vale para inteiros, mas nao para
+    # strings;
+    my $rep2 = $st1 * 3;
+    print "$rep2 " . "\\o" . "\n"; # Vai imprimir "0 \o" depois do erro
+
 }
 
 # Perl 5.10> <=> say bultin
