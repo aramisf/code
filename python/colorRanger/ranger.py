a82446dde2367d5dbb4d4a9ae115c81bef5501a8
aramisf <aramis.fernandes@gmail.com>
2011-09-26 10:33:13 -0300

    Support for global comparisons.
    More methods added to support local and global estimate. If more than one
    argument is passed through command line, the program creates objects and store
    their values.
    
    The goal is to be able to set speeds on a map, and compare different path
    speeds between each other, setting a maximum and minimum globals and locals.

--- python/colorRanger/ranger.py
+++ python/colorRanger/ranger.py
@@ -6,8 +6,8 @@ class Ranger(object):
 
     def __init__(self, inputFileFP,outputFileFP):
 
-        self.inputFileFP = inputFileFP
-        self.outputFileFP = outputFileFP
+        self.inputFileFP = open(inputFileFP, 'r')
+        self.outputFileFP = open(outputFileFP,'w')
         self.speedList = [ float(i.strip()) for i in self.inputFileFP.readlines() ]
 
         # Closing FP:
@@ -17,6 +17,9 @@ class Ranger(object):
         self.minSpeed = min(self.speedList)
         self.maxSpeed = max(self.speedList)
 
+    # Set speed list and color codes for this object:
+    def setMyLocalSpeedColors(self):
+
         # Because we need a zero:
         self.lowerBoundary = (self.minSpeed * 255) / self.maxSpeed
 
@@ -24,21 +27,51 @@ class Ranger(object):
         self.upperBoundary = 255
 
         # Now findind hexa values, corresponding to speed values:
-        self.colorSpeedList = []
+        self.localSpeedColorCodes = []
 
         # Filling up color hex codes list:
         for i in range(len(self.speedList)):
 
-            self.colorSpeedList.append(int(((self.speedList[i]*255)/self.maxSpeed)))
+            self.localSpeedColorCodes.append(int(((self.speedList[i]*255)/self.maxSpeed)))
 
 
-    def about(self):
+    # Set speed list and color codes for this object, considering global
+    # values:
+    def setMyGlobalSpeedColors(self,minGlobalSpeed,maxGlobalSpeed):
 
-        # Prints out information of interest:
+        # Because we need a zero:
+        self.lowerGlobalBoundary = (minGlobalSpeed * 255) / maxGlobalSpeed
+
+        # And this would be the maximum speed:
+        self.upperGlobalBoundary = maxGlobalSpeed
+
+        # Now findind hexa values, corresponding to speed values:
+        self.globalSpeedColorCodes = []
+
+        # Filling up color hex codes list:
         for i in range(len(self.speedList)):
 
-            # Into hex format, of course:
-            print "km/h: %.2f, cor: ff00%.2x%.2x" %\
-            (self.speedList[i],self.colorSpeedList[i],255-self.colorSpeedList[i])
+            self.globalSpeedColorCodes.append(int(((self.speedList[i]*255)/self.upperGlobalBoundary)))
+
+
+    # It will change according to argv size:
+    def about(self, mode='local'):
+
+        # When argv size is 1:
+        if (mode == 'local'):
+
+            for i in range(len(self.speedList)):
+
+                # Into hex format:
+                print "km/h: %.2f, cor: ff00%.2x%.2x" %\
+                (self.speedList[i],self.localSpeedColorCodes[i],255-self.localSpeedColorCodes[i])
+
+        elif (mode == 'global'):
+
+            for i in range(len(self.speedList)):
+
+                # Into hex format:
+                print "km/h: %.2f, cor: ff00%.2x%.2x" %\
+                (self.speedList[i],self.globalSpeedColorCodes[i],255-self.globalSpeedColorCodes[i])
 
 
