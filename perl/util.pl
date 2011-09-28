2191d27cfbe78d59319124640703c3fd987a07aa
aramisf <aramis.fernandes@gmail.com>
2011-09-28 00:51:08 -0300

    Removed !c code, on c branch.

--- perl/util.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl -w
-# util.pl - Funcoes de uso comum. A ideia eh que este arquivo seja um modulo e
-# q seja possivel importa-lo para uso em outros programas.
-# aramisf@github.com
-
-sub separador {
-
-    print "- - -\n";
-}
