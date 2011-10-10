385d0f301bb4fdc9982ae2cd58e54cdbf8618a45
aramisf <ashf03@c3sl.ufpr.br>
2011-10-09 23:15:53 -0300

    Added fork function example.

--- c/fork.c
+++ c/fork.c
@@ -8,32 +8,40 @@ int main (int argc, char **argv) {
 	int pid;
 
 	pid = fork();
+
 	if (pid < 0){
+
 		perror(argv[0]);
 		exit(0);
 	}
 
 	else if (pid != 0){
+
 		printf("Codigo executado no processo pai\n");
 		printf("Pai ID do Pai: %d\n",getpid());
 		printf("Pai ID do Filho: %d\n",pid);
 		printf("Endereco main : %x\n",&main);
 		execlp("./vet","vet",NULL);
 	}
+
 	else{
+
 		printf("Codigo executado pelo filho (else)\n");
 	}
 
 	if (pid == 0){
+
 		printf("Codigo executado pelo filho (pid == 0)\n");
 		printf("Filho PID do Pai: %d\n",getppid());
 		printf("Filho PID do Filho: %d\n",getpid());
 		printf("Endereco main : %x\n",&main);
 	}
+
 	else
+
 		printf("Codigo executado pelo pai\n");
+
 
 	printf("Mensagem final\n");
 	exit(0);
-	
 }
