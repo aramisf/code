fce9b371827f955b8420ae92eb353cd49d82a0c7
aramisf <aramis.fernandes@gmail.com>
2011-10-31 11:26:39 -0200

    [perl] Code identation.

--- perl/teach_yourself/sourcing.pl
+++ perl/teach_yourself/sourcing.pl
@@ -3,13 +3,13 @@
 use strict;
 use warnings;
 
-my $example = "/root"; # Try some other files here.
+my $example = "/root/source.conf"; # Try some other files here.
 open (my $conf, '<', $example) or die "oops - $!\n";
 
 while (<$conf>) {
-next if /^[\#\$]/;
-my @values = split (/=/, $_) or next;
-chomp ($ENV{$values[0]} = $values[1]);
+    next if /^[\#\$]/;
+    my @values = split (/=/, $_) or next;
+    chomp ($ENV{$values[0]} = $values[1]);
 }
 
 __END__
