3abc9bcccd0f2a8072788b02dfd3e4b187940c2a
aramisf <aramis.fernandes@gmail.com>
2011-11-03 00:25:19 -0200

    Added math_ops.
    This first one calculates log on any base.

---
+++ perl/teach_yourself/math_ops.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+# Using logarithms #
+
+# For log base 'e'
+#my $log_e = log VALUE;
+
+# For log base '10'
+#use POSIX qw(log_10);
+#my $log10 = log10(VALUE);
+
+# And for any base:
+sub malog {
+
+    my ($value, $base) = @_;
+
+    print "log($value)/log($base): ";
+    print log($value)/log($base);
+    print "\n\n";
+
+    print "log $value/log $base: ";
+    print log $value/log $base;
+    print "\n\n";
+
+    if (@_ == 1) {  # base 2 detected!
+        return log ($value);
+    }
+ 
+    if (@_ == 2) {
+        return log ($value)/log($base);
+    }
+ 
+    else  {
+        return 0;
+    }
+}
+
+my $v1 = sprintf ("%d",malog (1024,2));
+my $v2 = sprintf ("%d",malog (768,2));
+
+print "$v1\n";
+print "$v2\n";
