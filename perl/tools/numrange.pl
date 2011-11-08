5807fc6a44b1028785f7c2a7209c4f5c85ef9da6
aramisf <aramis.fernandes@gmail.com>
2011-11-08 10:43:44 -0200

    [perl] Added local variable.
    Now a list is returned. Maybe this is a better use of the function.

--- perl/tools/numrange.pl
+++ perl/tools/numrange.pl
@@ -10,17 +10,24 @@ sub hexa {
     my $min = shift || 0x0;
     my $max = shift || $min + 0xf;# Ok, just a simple range is enough too play.
 
+    my @result = ();                # The returned list;
     my $hexalen = length($max) - 2; # Ignoring the '0x'.
     my $binlen = $hexalen * 4;      # One hexa equals 4 binaries.
     my $declen = length(sprintf("%d",hex($max))); # Decimal length of max number
 
-    print sprintf("0x%.".$hexalen."x = %.".$declen."d = %.".$binlen."b\n",$_,$_,$_) foreach (hex($min)..hex($max));
+    foreach (hex($min)..hex($max)) {
 
+        push (@result, sprintf("0x%.".$hexalen."x ",$_)); 
+    }
+
+    #print sprintf("0x%.".$hexalen."x = %.".$declen."d = %.".$binlen."b\n",$_,$_,$_) foreach (hex($min)..hex($max));
+    return @result;
 }
 
 if ($ARGV[0] && $ARGV[1]) {
 
-    hexa($ARGV[0],$ARGV[1]);
+    my @lista = hexa($ARGV[0],$ARGV[1]);
+    print "Lista: @lista\n";
 }
 
 else {
