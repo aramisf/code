e33ec1692d5ad884fdae9c15594a08932bc283b8
aramisf <aramis.fernandes@gmail.com>
2012-01-11 04:47:19 -0200

    [perl] Ilegibilidade adicionada.
    Agora deu.

--- perl/teach_yourself/refs.pl
+++ perl/teach_yourself/refs.pl
@@ -64,12 +64,22 @@ my @a3d = (   [ {'a',1,'b',2},
 
 my $ra3d = \@a3d;
 
-print "1a Hash:\n";
+print "1a Hash (via referencia em 1o nivel):\n";
 print "$_: ${$ra3d}[0][0]{$_} " foreach keys ${$ra3d}[0][0];
 print "\n";
 
-print "2a Hash:\n";
-print "$_: ${$ra3d}[0][1]{$_} " foreach keys ${$ra3d}[0][0];
+print "2a Hash (via referencia no 2o nivel):\n";
+print "$_: $a3d[0]->[1]{$_} " foreach keys $a3d[0]->[1];
+print "\n";
+
+# Aqui esta escrito quase que da mesma forma do print anterior, mas este aqui eh
+# muito menos legivel. Note q nao se usa a referencia nesta linha:
+print "3a Hash (via referencia no 3o nivel do vetor):\n"; # Praticamente ilegivel o.O
+print "$_: ${${$a3d[1]}[0]}{$_} " foreach keys ${$a3d[1]}[0];
+print "\n";
+
+print "4a Hash (via referencia no 3o nivel da ref):\n"; # Praticamente ilegivel o.O [2]
+print "$_: ${${$ra3d}[1]}[1]{$_} " foreach keys ${$ra3d}[1][1];
 print "\n";
 
 print "Valor da chave i, localizada na primeira hash do 3o vetor do vetor principal: ${$ra3d}[2][0]{'i'}\n";
