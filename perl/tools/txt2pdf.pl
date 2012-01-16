d54b9168e1bacb5223ebaab5f529d3ca9cfd59d3
aramisf <aramis.fernandes@gmail.com>
2012-01-16 17:09:38 -0200

    [perl] Adicionado txt2pdf.pl

---
+++ perl/tools/txt2pdf.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+# Programa que converte um arquivo txt simples em um arquivo pdf. Passe o
+# arquivo texto como parametro do script.
+
+use strict;
+use warnings;
+
+use PDF::API2::Simple;
+
+if ( -f $ARGV[0]) {
+
+    open(ARQ,'<',$ARGV[0]);
+
+    # Criando um arquivo de exemplo
+    my $pdf = PDF::API2::Simple->new(file => "saida.pdf");
+
+    $pdf->add_font("Verdana");  # Se nao definir uma fonte, da erro.
+    $pdf->add_page();           # Cria a 1a pagina;
+
+
+    while (my $linha = <ARQ>) {
+
+        print "Enviando linha: $linha\n";
+        $pdf->text($linha, autoflow => 'on');
+    }
+
+    $pdf->save();
+}    
