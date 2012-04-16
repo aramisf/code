14a3dd3df1aeec9702713189614059c74c66a50f
aramisf <aramis.fernandes@gmail.com>
2012-04-16 15:37:10 -0300

    [perl] Finalizando.
    Altos testes com casamento de padroes. Uma boa lida nos comentarios esclarece
    bem o funcionamento e a administracao do perl sobre suas variaveis padrao.

--- perl/teach_yourself/regex.pl
+++ perl/teach_yourself/regex.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 use strict;
-use warnings;
+#use warnings;
 
 # Testes basicos com expressoes regulares em perl.
 
@@ -23,11 +23,52 @@ print "Str: $f1, -@resto-\n";
 print "Resto: ";
 print "$_\n" foreach @resto;
 
+print '-'x15;
+print "\n";
 
 my @str2 = ('asdf', '5678', 'qwer', '1213', 'zxcv', '1234');
+
+print "Agora testando com a lista [@str2]\n";
 foreach (@str2) {
 
-    my $saida = /([0-9]+)/;
-    print "saida: $1\n" if $saida ne '';
+    print "Testando para $_:\n\n";
+    if (/[a-z]+/){
+
+        # Duas formas para testar.
+        # XXX: mantenha uma comentada:
+
+        #my $s1 = $_;                               # Pode usar $s1 ou $_
+
+        #my ($s2, $s3) = $_ =~ /([a-z]{2})(.*)/;    # Equivalente aa linha abaixo
+        my ($s2, $s3) = /([a-z]{2})(.*)/;
+        print "1o round (=):  s2: $s2 s3: $s3\n\n";
+
+
+
+        # A expressao abaixo nao casa:
+        #my ($s2, $s3) =~ /([a-z]{2})(.*)/;
+
+        # Porem estas aqui casam:
+        #my ($s2, $s3) = $s1 =~ /([a-z]{2})(.*)/;
+        #my ($s2, $s3) = $_ =~ /([a-z]{2})(.*)/;
+        #print "2o round (=~): \$1: $1, \$2: $2\n\n";
+
+        # XXX Conclusao: $1 e $2 soh funcionam quando se usa $var = $_ =~ /regex/
+    }
+
+    elsif (/[0-9]+/) {
+
+        # Valores numericos:
+        #my $saida = /([0-9]+)/;        # Sem () retorna a resposta logica se
+                                        # casou ou nao a regex.
+
+        my ($saida) = /([0-9]+)/;       # Com parentesis, retorna o padrao casado
+        print "1o round \$saida: $saida\n\n";
+
+        #my ($saida) = $_ =~ /([0-9]+)/; # Parentesis faz o mesmo que acima
+        #print "2o round \$saida: $saida\n";
+        #print "2o round \$1: $1\n\n";
+    }
+
 }
 
