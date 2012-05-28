fabf2bc9fecb5cf06e092e356c68fb8a89564fe3
aramisf <aramis.fernandes@gmail.com>
2012-05-28 15:27:17 -0300

    [c] Adicionado ##.c
    Operador em C, chamado concatenador de tokens.

---
+++ c/##.c
@@ -0,0 +1,20 @@
+/* por pzanoni: https://github.com/pzanoni */
+#include <stdio.h>                                                               
+                                                                                 
+#define soma(tipo) \                                                             
+tipo soma_##tipo(tipo a, tipo b) { return a + b; }                               
+                                                                                 
+soma(int)                                                                        
+soma(char)                                                                       
+soma(double)                                                                     
+                                                                                 
+int main(void) {                                                                 
+    int ia = 0, ib = 1;                                                      
+    char ca = 0, cb = 1;                                                     
+    double da = 0.0, db = 1.0;                                               
+                                                                             
+    printf("%d\n", soma_int(ia, ib));                                        
+    printf("%c\n", soma_char(ca, cb));                                       
+    printf("%f\n", soma_double(da, db));                                     
+    return 0;                                                                
+}
