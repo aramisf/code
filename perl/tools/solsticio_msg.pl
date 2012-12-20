5248cddba11cf7356caaf06c7725a8327c7f3cea
aramisf <aramis.fernandes@gmail.com>
2012-12-20 00:23:03 -0200

    [perl] Mais uma correcao.
    Agora mostra o valor absoluto dos dias que ja se passaram, a numeracao esta de
    acordo com a mensagem.

--- perl/tools/solsticio_msg.pl
+++ perl/tools/solsticio_msg.pl
@@ -36,7 +36,7 @@ elsif ($dias == 0) {
     $msg = "O solsticio de verao eh hj!";
 }
 else {
-    $msg = "Jah se passou(aram) $dias dia(s) desde o solsticio de verao";
+    $msg = "Jah se passou(aram) ".abs($dias)." dia(s) desde o solsticio de verao";
 }
 print "$msg\n";
 exec ("purple-remote setstatus?message=\"$msg - via perl script at http://0.mk/8c08d\"");
