810970db1e8712f71de362783387ee97cdd567d6
aramisf <aramis.fernandes@gmail.com>
2012-12-19 23:07:12 -0200

    [perl] Atualizando solsticio.
    Agora o script esta preparado :P

--- perl/tools/solsticio_msg.pl
+++ perl/tools/solsticio_msg.pl
@@ -10,15 +10,25 @@
 
 # Se o ceu estiver encoberto neste dia, fodeu xD
 
-use Date::Calc qw(Delta_Days Today);
+use Date::Calc qw(Delta_Days Localtime Today);
+my $dias,$msg;
 
-my @hj = Today([$gmt]);
+my @hj = localtime;
+@hj = ($hj[5]+1900,$hj[4]+1,$hj[3]);
+
 my @solsticio = (2012,12,21);
 
 my $dias = Delta_Days(@hj, @solsticio);
 
-my $msg = "Faltam $dias dias para o proximo solsticio de verao";
-
+if ($dias > 0) {
+    $msg = "Faltam $dias dias para o proximo solsticio de verao";
+}
+elsif ($dias == 0) {
+    $msg = "O solsticio de verao eh hj!";
+}
+else {
+    $msg = "Jah se passaram $dias dias desde o solsticio de verao";
+}
 print "$msg\n";
 exec ("purple-remote setstatus?message=\"$msg - via perl script at http://0.mk/8c08d\"");
 
