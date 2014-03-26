f8ccc08c0673820a88798fddb62309aeeb7fcab0
Aramis Fernandes <aramis.fernandes@gmail.com>
2014-03-26 18:21:35 -0300

    Adicionando demo_app

---
+++ railstutorial/demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '696593aa83e50701b4bd8740bd9e7bb5b5b5af7125931d3b4fbf7415b0974a584e0ae34c396edcbb2c7b29c0e0df2ee97076b8770889325353054e1c70545b57'
