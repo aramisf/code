f0a009061479cb07dc86190791f512dba41a6b06
Aramis Fernandes <aramis.fernandes@gmail.com>
2014-03-27 01:16:21 -0300

    Exercicio 2.
    String interpolation.

--- railstutorial/sample_app/spec/requests/static_pages_spec.rb
+++ railstutorial/sample_app/spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -8,7 +10,7 @@ describe "StaticPages" do
     end
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -27,7 +29,7 @@ describe "StaticPages" do
     end
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -38,7 +40,18 @@ describe "StaticPages" do
     end
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact_us'
+      expect(page).to have_content('Contact Us')
+    end
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact_us'
+      expect(page).to have_title("#{base_title} | Contact Us")
     end
   end
 end
