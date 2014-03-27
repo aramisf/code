96eed0d3d363e4505a1cd776ce11bd2141d06c7b
Aramis Fernandes <aramis.fernandes@gmail.com>
2014-03-26 22:54:23 -0300

    Adicionando sample_app.

---
+++ railstutorial/sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'ac4b81ed56d5e1a77c1947ec45a0e62c3424cf9e999740ae7dcb7f6ec6c765a5809621e86fb70d3265aba08aa6559ab13f95cb93770a0846c0da860fcd5c1259'
