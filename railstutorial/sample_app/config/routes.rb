5e37f606e6bfd567e11a1cbc976d1244710e0e73
Aramis Fernandes <aramis.fernandes@gmail.com>
2014-03-28 01:55:05 -0300

    Finalizado o cap 5.

--- railstutorial/sample_app/config/routes.rb
+++ railstutorial/sample_app/config/routes.rb
@@ -1,9 +1,13 @@
 SampleApp::Application.routes.draw do
 
-  #match '/', to: 'static_pages#home', via: 'get', as: 'root'
-  match '/help', to: 'static_pages#help', via: 'get'
-  match '/about', to: 'static_pages#about', via: 'get'
+  get "users/new"
+  #match '/',       to: 'static_pages#home',    via: 'get', as: 'root'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+
+  match '/signup',  to: 'users#new',            via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- railstutorial/sample_app/spec/requests/static_pages_spec.rb
+++ railstutorial/sample_app/spec/requests/static_pages_spec.rb
@@ -5,11 +5,36 @@ describe "StaticPages" do
 
   subject {page}
 
+  # definindo comportamento padrao para os testes abaixo:
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+  # verificando se os links estao funcionando:
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact Us'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
+  end
+
   describe "Home page" do
     before {visit root_path}
+    let(:heading)   {'Sample App'}
+    let(:page_title){''}
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
+    it_should_behave_like "all static pages" 
+    #it { should have_content('Sample App') }
+    #it { should have_title(full_title('')) }
     it { should_not have_title("| Home") }
   end
 
@@ -23,23 +48,35 @@ describe "StaticPages" do
 
   describe "Help page" do
     before {visit help_path}
+    let(:heading)   {'Help'}
+    let(:page_title){'Help'}
 
-    it { should have_content('Help')}
-    it { should have_title(full_title("Help"))}
+    it_should_behave_like "all static pages"
+
+    #it { should have_content('Help')}
+    #it { should have_title(full_title("Help"))}
     #it { should have_title("#{base_title} | Help")}
   end
 
   describe "About page" do
     before {visit about_path}
+    let(:heading)   {'About'}
+    let(:page_title){'About'}
 
-    it { should have_content('About Us') }
-    it { should have_title(full_title("About Us")) }
+    it_should_behave_like "all static pages"
+
+    #it { should have_content('About Us') }
+    #it { should have_title(full_title("About Us")) }
   end
 
   describe "Contact page" do
     before {visit contact_path}
+    let(:heading)   {'Contact'}
+    let(:page_title){'Contact'}
 
-    it { should have_content('Contact Us') }
-    it { should have_title(full_title("Contact Us")) }
+    it_should_behave_like "all static pages"
+
+    #it { should have_selector('h1', text: 'Contact Us') }
+    #it { should have_title(full_title("Contact Us")) }
   end
 end
