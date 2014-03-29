df87fc1cf4d0f0a1634831d06bf33e9be38282ba
Aramis Fernandes <aramis.fernandes@gmail.com>
2014-03-29 19:53:40 -0300

    Fim cap 6.

--- railstutorial/sample_app/spec/models/user_spec.rb
+++ railstutorial/sample_app/spec/models/user_spec.rb
@@ -33,7 +33,7 @@ describe User do
   describe "when email format is invalid" do
     it "should not be valid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+                     foo@bar_baz.com foo@bar+baz.com foo@bar..com]
 
       addresses.each do |invalid_address|
         @user.email = invalid_address
@@ -95,4 +95,15 @@ describe User do
       specify { expect(user_for_invalid_password).to be_false }
     end
   end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
 end
