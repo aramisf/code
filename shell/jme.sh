6eae7f1315540998a8a665e68774438dbb116df5
aramisf <aramis.fernandes@gmail.com>
2014-06-16 14:56:10 -0300

    [shell] +JME

---
+++ shell/jme.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# JME - JPEG Metadata Eraser/Editor
+
+# Tem q passar um arquivo como parametro
+[ -n $1 ] && [ -f $1 ] || exit 1
+
+
+# Recomenda-se fazer bkp dos dados que vc queira guardar:
+exiv2 -d e $1
+
+# Agora adiciona apenas informacoes relevantes:
+exiv2 -M"add Exif.Image.Make TekPix" \
+      -M"add Exif.Image.Model TekPix i-HD18" \
+      -M"add Exif.Photo.UserComment Ascii Vc acredita mesmo nesses dados?" \
+      -M"add Exif.Image.DateTime 1970:01:01 00:00:00" $1
+
