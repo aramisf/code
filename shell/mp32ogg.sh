21580b93c7fa1f1c7bc7320d41bc556a5d3ad231
aramisf <ashf03@c3sl.ufpr.br>
2014-08-08 13:26:57 -0300

    [shell] mp32ogg.sh++

--- shell/mp32ogg.sh
+++ shell/mp32ogg.sh
@@ -6,12 +6,17 @@ mpg321 -w raw $mp3_input && oggenc raw -o $ogg_output
 # Some more possibilities, according to what you've got:
 ffmpeg -i input.mp3 -c:a libvorbis -q:a 4 output.ogg
 
-#To do a whole directory full of MP3s:
+# To do a whole directory full of MP3s:
 
-for f in ./*.mp3; do ffmpeg -i "$f" -c:a libvorbis -q:a 4 "${f/%mp3/ogg}"; done
+for f in ./*.mp3; do
+
+  ffmpeg -i "$f" -acodec libvorbis -aq 4 "${f/%mp3/ogg}"
+done
 
 # Recursively, with find:
 find . -type f -name '*.mp3' -exec bash -c 'ffmpeg -i "$0" -c:a libvorbis -q:a
 4 "${0/%mp3/ogg}"' '{}' \;
 
+# On older versions of ffmpeg, you may need to use -acodec and -aq instead of
+# -c:a and -q:a.
 # thanks to: http://superuser.com/questions/273797/convert-mp3-to-ogg-vorbis
