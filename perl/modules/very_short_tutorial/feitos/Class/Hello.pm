c43eeb5eba7183d2e67d78dd2a4e61a3c5e927e5
aramisf <aramis.fernandes@gmail.com>
2015-08-26 21:36:04 -0300

    backing up old exercises

--- perl/modules/very_short_tutorial/feitos/Class/Hello.pm
+++ perl/modules/very_short_tutorial/feitos/Class/Hello.pm
@@ -4,28 +4,53 @@ package Hello;
 use Carp;
 
 sub new {
-  print "\@_ de new eh: '@_'\n";
+  #print "\@_ de new eh: '@_'\n";
   my ($package, $msg) = @_;
   my %hash;
   $msg = "Hello, world" unless defined $msg;
-  %hash = (MESSAGE => "$msg\n");
+  #%hash = (MESSAGE => "$msg\n");
+  %hash = (DEFAULT => "$msg");
   bless \%hash => $package;
 }
 
 sub greet {
-  my ($self) = @_;
-  print $self->{MESSAGE};
-  $self->{MESSAGE};
+  my ($self)  = @_;
+  print "Dentro de greet:\n";
+
+  my $msg;
+
+  if ($self->{MESSAGE}) {
+
+    $msg  = $self->{MESSAGE};
+  }
+  elsif ($self->{DEFAULT}) {
+
+    $msg  = $self->{DEFAULT};
+  }
+  print "msg: $msg\n";
+  #print "Saindo de greet:\n";
+  $msg;
 }
 
 sub setmessage {
-  print "\@_ em setmessage: @_\n";
-  my ($self, $message) = @_;
-  print "\$self em setmessage: $self\n";
-  croak("missing argument to setmessage") unless defined $message;
+  print "\@_ em setmessage: '@_'\n";
+  my ($self, $msg) = @_;
+  #print "\$self em setmessage: $self\n";
+
+  # Ex6
+  if ($msg) {
+
+    $self->{MESSAGE}  = $msg;
+  }
+  else  {
+
+    $self->{MESSAGE}  = $self->{DEFAULT};
+  }
+
+  #croak("missing argument to setmessage") unless defined $message;
   #die("missing argument to setmessage") unless defined $message;
 
-  $self->{MESSAGE} = $message;
+  $self->{MESSAGE};
 }
 
 1;
